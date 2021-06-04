32a37380595ebf63aa9f753b8db3f584e7ef9b13
YerGuerrero <guerreroyerlyn221113@gmail.com>
2021-06-04 09:20:49 -0600

    feat: Se añade 5 cantones de Costa Rica

--- src/services/MyMap.js
+++ src/services/MyMap.js
@@ -3,7 +3,7 @@
 
 export default class MyMap {
   constructor(initCenter = [-39.11133, 36.66842], style = 'mapbox://styles/mapbox/dark-v9') {
-    mapboxgl.accessToken = process.env.MAPBOX_TOKEN || 'your token';
+    mapboxgl.accessToken = process.env.MAPBOX_TOKEN || 'pk.eyJ1IjoieWVyZ3VlcnJlcm8iLCJhIjoiY2tudWs1YWgyMGJwdTJxcnF2a3RrNXJxZCJ9.MqMJE2PNtDB0o1nZKfnf_w';
 
     this.map = new mapboxgl.Map({
       container: 'map',
